2a13da59f4cd4ea930a1e219f148bf83f9d7020e
Anis-khemiri <aniskhemiri6@gmail.com>
2024-01-17 17:02:36 +0100

    add section fetch data

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
 import Banner from './components./Banner'
+import Explore from './components./Explorer'
 
 export default function Home() {
   return (
    <main>
     <Banner />
+    <Explore />
    </main>
   )
 }
---
+++ app/types/app.ts
@@ -0,0 +1,8 @@
+export type ExploreItem = {
+    img: string;
+    location: string;
+    distance: string;
+}
+
+
+export type ExploreData = ExploreItem[];
