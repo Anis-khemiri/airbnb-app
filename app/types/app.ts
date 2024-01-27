ab6cc969066c9d70c06966918604e399c96a5758
Anis-khemiri <aniskhemiri6@gmail.com>
2024-01-27 17:35:00 +0100

    fetch data

--- app/types/app.ts
+++ app/types/app.ts
@@ -11,8 +11,20 @@ export type LiveItem = {
 }
 
 
+export type ListingCardItem = {
+    img: string,
+    location: string,
+    title: string,
+    description: string,
+    star: number,
+    price: string,
+    total: string,
+    long: number,
+    lat: number,
 
+}
 
 
 export type ExploreData = ExploreItem[];
 export type LiveData = LiveItem[];
+export type SearchResultData = ListingCardItem[];
